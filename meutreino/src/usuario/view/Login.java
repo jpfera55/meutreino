0bbaeee146f979f875398fa0749fc882f100304f
allanalves1511@gmail.com <allanalves1511@gmail.com@dc7a4112-6983-eab5-bdb6-ee62a0798e79>
2014-04-11 23:38:01 +0000

    criação da prototipo da tela de login

---
+++ meutreino/src/usuario/view/Login.java
@@ -0,0 +1,88 @@
+package usuario.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
+import java.awt.Font;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JButton btnCriarConta;
+	private JLabel lblAbraSuaConta;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 382, 229);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(116, 39, 134, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblLogin = new JLabel("Login :");
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblLogin.setBounds(65, 38, 46, 19);
+		contentPane.add(lblLogin);
+		
+		JLabel lblSenha = new JLabel("Senha:");
+		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblSenha.setBounds(65, 74, 46, 14);
+		contentPane.add(lblSenha);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnOk.setBounds(260, 72, 46, 23);
+		contentPane.add(btnOk);
+		
+		btnCriarConta = new JButton("Criar Conta");
+		btnCriarConta.setBounds(102, 139, 164, 23);
+		contentPane.add(btnCriarConta);
+		
+		lblAbraSuaConta = new JLabel("Abra sua conta");
+		lblAbraSuaConta.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblAbraSuaConta.setBounds(126, 104, 106, 24);
+		contentPane.add(lblAbraSuaConta);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(116, 73, 134, 20);
+		contentPane.add(passwordField);
+	}
+}
